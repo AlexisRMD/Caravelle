cd373ddd29f42888bd9324f14572847784f8e59c
Anne-Sophie Koch <anne.sophie.koch54@gmail.com>
2022-09-24 01:08:04 +0200

    Draggables stones

---
+++ Assets/Scripts/SelectStone.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectStone : MonoBehaviour
+{
+    [SerializeField] private float mouseDragPhysicsSpeed = 10;
+    [SerializeField] private float mouseDragSpeed = 0.1f;
+
+
+    private Camera mainCamera;
+    private Vector3 velocity = Vector3.zero;
+    private WaitForFixedUpdate waitForFixedUpdate = new WaitForFixedUpdate();
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider != null && hit.collider.gameObject.CompareTag("Draggable"))
+                {
+                    hit.collider.gameObject.transform.position = new Vector3(hit.collider.gameObject.transform.position.x, 0.5f, hit.collider.gameObject.transform.position.z);
+                    StartCoroutine(DragUpdate(hit.collider.gameObject));
+                }
+            }
+        }
+    }
+
+    private IEnumerator DragUpdate(GameObject clickedObject)
+    {
+        float initialDistance = Vector3.Distance(clickedObject.transform.position, mainCamera.transform.position);
+        clickedObject.TryGetComponent<Rigidbody>(out var rb);
+        if (rb != null) rb.freezeRotation = true; rb.constraints = RigidbodyConstraints.FreezePositionY;
+        while (Input.GetMouseButton(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (rb != null)
+            {
+                Vector3 direction = ray.GetPoint(initialDistance) - clickedObject.transform.position;
+                rb.velocity = direction * mouseDragPhysicsSpeed;
+                yield return waitForFixedUpdate;
+            }
+            else
+            {
+                clickedObject.transform.position = Vector3.SmoothDamp(clickedObject.transform.position,
+                    ray.GetPoint(initialDistance), ref velocity, mouseDragSpeed);
+                yield return null;
+            }
+        }
+
+        if (rb != null) rb.velocity = Vector3.zero; rb.freezeRotation = false; rb.constraints = RigidbodyConstraints.None;
+        
+    }
+}
